70be69c8acdb3bdde15c9608e3f24f01ac93b70e
abhinav kumar <abhinav.mg.k@gmail.com>
2024-12-24 13:45:54 +0530

    updates done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Analyst-Exam-Question-Answer-Creator",
+    version= '0.0.0',
+    author="Abhinav Kumar",
+    author_email="abhinav.mg.k@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
